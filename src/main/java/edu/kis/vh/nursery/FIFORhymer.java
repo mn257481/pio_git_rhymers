644be95935be8c9ae8bff7d8f709b5bc8b683d36
Kacper Banert <255561@edu.p.lodz.pl>
2026-03-26 13:32:43 +0100

    Dodanie override i modyfikatora final wraz z wydzieleniem stalych

--- src/main/java/edu/kis/vh/nursery/FIFORhymer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORhymer.java
@@ -2,7 +2,7 @@ package edu.kis.vh.nursery;
 
 public class FIFORhymer extends DefaultCountingOutRhymer {
 
-    public DefaultCountingOutRhymer countingOutRhymer = new DefaultCountingOutRhymer();
+    public final DefaultCountingOutRhymer countingOutRhymer = new DefaultCountingOutRhymer();
 
     @Override
     public int countOut() {
