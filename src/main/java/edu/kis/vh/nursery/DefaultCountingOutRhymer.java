bfa9fcba57f909faa06cd1f1d67bae6724d0c989
Kacper Banert <255561@edu.p.lodz.pl>
2026-03-30 19:22:07 +0200

    Drobne poprawki w zmianie zmiennej capacity

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -4,8 +4,8 @@ public class DefaultCountingOutRhymer {
 
     private static final int EMPTY = -1;
     private static final int DEFAULT = -1;
-    private static final int CAPACITY = -1;
-    private int[] numbers = new int[12];
+    private static final int CAPACITY = 12;
+    private int[] numbers = new int[CAPACITY];
 
     public int total = EMPTY;
 
