d7731a47dbb25e4182486ce3a566ff21ecefb593
mn257481 <257481@edu.p.lodz.pl>
2026-03-31 12:57:22 +0200

    17. Dodano JavaDoc do DefaultCountingOutRhymer

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -1,30 +1,55 @@
 package edu.kis.vh.nursery;
 
+/**
+ * Default rhymer implementation.
+ * Acts like a LIFO stack.
+ */
 public class DefaultCountingOutRhymer {
 
     private int[] numbers = new int[12];
 
     public int total = -1;
 
+    /**
+     * Puts the number inside the rhymer.
+     * Doesn't do anything if the rhymer is full.
+     * @param in Number to put in
+     */
     public void countIn(int in) {
         if (!isFull())
             numbers[++total] = in;
     }
 
+    /**
+     * Checks if there are any numbers in the rhymer.
+     * @return Whether there are any numbers in the rhymer.
+     */
     public boolean callCheck() {
         return total == -1;
     }
 
+    /**
+     * Checks if the rhymer is full.
+     * @return Whether the rhymer is full or not.
+     */
     public boolean isFull() {
         return total == 11;
     }
 
+    /**
+     * Peeks the last number.
+     * @return The last number in the rhymer or -1 if there are no numbers.
+     */
     protected int peekaboo() {
         if (callCheck())
             return -1;
         return numbers[total];
     }
 
+    /**
+     * Returns the last number and decrements the total amount of numbers.
+     * @return Returns the last number or -1 if there are no numbers.
+     */
     public int countOut() {
         if (callCheck())
             return -1;
